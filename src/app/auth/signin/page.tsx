b6ac410595f709cc4431f97db2031f4cdd77dc02
hichamelmir <blockhicham@gmail.com>
2025-02-15 22:36:07 +0100

    update auth

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -42,7 +42,7 @@ const SignIn: React.FC = () => {
       
     }
     try {
-      const response = await fetch('http://localhost:3333/auth/login', {
+      const response = await fetch('https://hi-chshop2025-nestjs.onrender.com/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
